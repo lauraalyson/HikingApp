58287e53086a756f07efeb431a65f03fdd965c93
lauraalyson <lauraalyson3@gmail.com>
2021-11-22 17:34:20 -0500

    initial

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { Dashboard, Place } from './screens'
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer } from '@react-navigation/native'
+import Tabs from './navigation/tabs'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createStackNavigator()
+
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{
+					headerShown: false,
+				}}
+				initialRouteName={'Dashboard'}>
+				<Stack.Screen name='Dashboard' component={Tabs} />
+
+				<Stack.Screen name='Place' component={Place} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ ReadMe.md
@@ -0,0 +1,7 @@
+# Expo Hiking App
+
+- Fork and Clone Repo
+- `npm install`
+- `npm start`
+- `expo start`
+  
